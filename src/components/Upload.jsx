9d5dbb13808d29594a74d59fa2d5ef083f0bce79
Susmitaa-Das <susmitadas2003hasi2021@gmail.com>
2023-05-18 23:57:48 +0530

    react app

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,4 +1,4 @@
-import { Button, Container, HStack, Input, VStack } from '@chakra-ui/react'
+import { Button, Container, HStack, Input, VStack, color } from '@chakra-ui/react'
 import React from 'react'
 import { AiOutlineCloudUpload } from 'react-icons/ai'
 
@@ -9,7 +9,17 @@ const Upload = () => {
 
         <form>
             <HStack>
-                <Input required type={'file'} />
+                <Input required type={'file'} css={{
+                    "&::file-selectoe-button":{
+                        border:'none',
+                        width:'calc(100% + 36px)',
+                        height:'100%',
+                        marginLeft:'-18px',
+                        color:'purple',
+                        backgroundColor:'white',
+                        cursor:'pointer',
+                    }
+                }}/>
                 <Button colorScheme={'purple'} type={'submit'}>Upload</Button>
             </HStack>
         </form>
